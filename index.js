9ad7253a94672410960d3ddfabaae4225f68a3ee
최재민 <colajeamin@gmail.com>
2022-08-13 10:55:01 +0900

    :building_construction: 봇 기본 코드 생성

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === '야') {
+		await interaction.reply('뭐 임마!');
+	}
+});
+
+
+// Login to Discord with your client's token
+client.login(token);
